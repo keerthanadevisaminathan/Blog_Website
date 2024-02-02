8231ed20f20df93fd14b7c820547a845698ca6af
KEERTHANA DEVI <keerthanadevis.21cse@kongu.edu>
2024-02-02 18:48:22 +0530

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
-const app= express();
-app.listen(3000,()=>{
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const MONGO_URI = process.env.MONGO;
+
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('MongoDB is connected');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
+const app = express();
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000!!!');
-}
-    );
+});
