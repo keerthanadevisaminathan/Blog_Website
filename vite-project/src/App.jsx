346b65e3338eddaf4e15cf3e4a8ab06f1d4dbc89
KEERTHANA DEVI <keerthanadevis.21cse@kongu.edu>
2024-02-02 11:20:06 +0530

    Routes created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Project from './pages/Project';
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-500">
-      Hello world!
-    </h1>
-  ) 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </Router>
+  );
 }
